de834026479f7b9ee037ed58ca1620ebc752c7a4
warnov <warnov@gmail.com>
2019-08-22 14:38:52 -0500

    first commit and mvp ready

---
+++ Lnkexecutor.CoreConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lnkexecutor.CoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            //   /C is mandatory to carry out the command specified by the arg string and then terminates
+            startInfo.Arguments = $"/C {args[0]}";//put in the args line the name/path of the *.lnk to execute
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+    }
+}
